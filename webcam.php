e6e2c2ac8a195e1a545fd812f4e69b6ed84e8a25
hphuc4244 <39155046+hphuc4244@users.noreply.github.com>
2019-05-10 05:15:52 +0700

    a

--- webcam.php
+++ webcam.php
@@ -1,7 +1,5 @@
 <?php 
         $location = "/home/hoangphuc/ftp/files/camera/1500086";
-        $uold     = umask(0);
-
-        mkdir($location,0777,true);
-        umask($uold);
+        mkdir($location);
+        
     ?>
