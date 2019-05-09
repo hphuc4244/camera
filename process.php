33c3a906aa5fb4780160f6498c442f17f779240f
hphuc4244 <39155046+hphuc4244@users.noreply.github.com>
2019-05-09 20:30:44 +0700

    a

--- process.php
+++ process.php
@@ -1,34 +1,6 @@
 <?php
 
-	@$con = mysqli_connect('localhost','root','123456','qlkhuonmat');
-	session_start();
-	if (isset($_POST['login']))
-	{
-		if(empty($_POST['usr'])|| empty($_POST['pass']))
-		{
-			a
-		}
-		else
-		{
-			$sql = "select * from nguoidangky where MaSo = ".$_POST['usr']." and MatKhau = '".$_POST['pass']."'";
-			$result = $con->query($sql);
-			if ($result->num_rows > 0)
-			{
-				
-						$_SESSION['User']=$_POST['usr'];
-						header("location:http://ec2-18-188-71-150.us-east-2.compute.amazonaws.com/webcam.php");
-					
-				
-			}
-			else
-			{
-				header("location:Login.php?Invalid=Please Enter");
-			}
-		}
-	}
-	else
-	{
-		
-	}
+	$con = new mysqli('localhost','root','123456');
+	echo "thanh cong";
 	mysqli_close($con);
 ?>
