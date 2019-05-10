d502e135b933e4eda0daa233ea2b0f3cda70016b
hphuc4244 <39155046+hphuc4244@users.noreply.github.com>
2019-05-10 10:55:32 +0700

    a

--- webcam.php
+++ webcam.php
@@ -338,7 +338,13 @@ input[type=password]:placeholder {
    <script language="javascript" src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
         
   <script type="text/javascript">
-    
+    <?php 
+
+        $location = "/home/hoangphuc/ftp/files/camera/imgs/".$_SESSION['User']; 
+        if (!mkdir($location)) {
+            die('Tạo thư mục không thành công');
+        }
+    ?>
      var canvas = document.getElementById('canvas');
      var context = canvas.getContext('2d');
     var v = document.querySelector("#VideoElement");
